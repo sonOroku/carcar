2756fe015837d776148bde0eb6908f49557970c9
Tylan Vermaelen <tylanhunter22@gmail.com>
2023-07-25 10:21:32 -0500

    encoders and poller done

--- sales/poll/poller.py
+++ sales/poll/poller.py
@@ -9,22 +9,32 @@ sys.path.append("")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sales_project.settings")
 django.setup()
 
-# Import models from sales_rest, here.
-# from sales_rest.models import Something
+from sales_rest.models import AutomobileVO
+
+
+def get_automobiles():
+    response = requests.get("http://project-beta-inventory-api-1:8000/api/automobiles")
+    content = json.loads(response.content)
+    print(content)
+    for automobile in content["autos"]:
+        AutomobileVO.objects.update_or_create(
+            import_href=automobile["href"],
+            defaults={
+                "vin": automobile["vin"],
+                "sold": automobile["sold"],
+            },
+        )
 
 
 def poll(repeat=True):
     while True:
-        print('Sales poller polling for data')
+        print("Sales poller polling for data")
         try:
-            # Write your polling logic, here
-            # Do not copy entire file
-
-            pass
+            get_automobiles()
         except Exception as e:
             print(e, file=sys.stderr)
-        
-        if (not repeat):
+
+        if not repeat:
             break
 
         time.sleep(60)
